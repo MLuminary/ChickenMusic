dd31f2e6f3a6c744bce8b2079493f955e126aadc
MLuminary <418037025@qq.com>
2018-05-30 10:20:36 +0800

    jsonp跨域拿recommend页面数据

---
+++ src/common/js/jsonp.js
@@ -0,0 +1,26 @@
+import originJSONP from 'jsonp'
+
+export default function jsonp(url, data, option) {
+  // 拼接 url
+  url += (url.indexOf('?') < 0 ? '?' : '&') + param(data)
+
+  return new Promise((resolve, reject) => {
+    originJSONP(url, option, (err, data) => {
+      if (!err) {
+        resolve(data)
+      } else {
+        reject(err)
+      }
+    })
+  })
+}
+
+// 将对象数据转为 location.search 的数据格式
+function param(data) {
+  let url = ''
+  for (var k in data) {
+    let value = data[k] !== undefined ? data[k] : ''
+    url += `&${k}=${encodeURIComponent(value)}`
+  }
+  return url ? url.substring(1) : ''
+}
