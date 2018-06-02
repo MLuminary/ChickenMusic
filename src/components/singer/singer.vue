4cdbe4fcb287c60eb5321a81d129b4cc7406a9b3
MLuminary <418037025@qq.com>
2018-06-02 23:38:29 +0800

    获取歌手数据

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,5 +1,32 @@
 <template>
-  <h1>
-    singer
-  </h1>
+  <div class="singer">
+
+  </div>
 </template>
+
+<script type="text/ecmascriot-6">
+import { getSingerList } from 'api/singer'
+export default {
+  data() {
+    return {}
+  },
+  mounted() {
+    this._getSingerList()
+  },
+  methods: {
+    _getSingerList() {
+      getSingerList().then(res => {
+        console.log(res)
+      })
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped rel="stylesheet/stylus">
+.singer
+  position fixed
+  top 88px
+  bottom 0
+  width 100%
+</style>
