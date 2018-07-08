1a1af7f5926a8d731ee9408e1765331bdc0751a0
MLuminary <418037025@qq.com>
2018-07-08 23:31:25 +0800

    searchbox完成

--- src/common/js/util.js
+++ src/common/js/util.js
@@ -2,6 +2,7 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
+// 洗牌算法
 export function shuffle(arr) {
   let _arr = arr.slice()
   for (let i = 0; i < _arr.length; i++) {
